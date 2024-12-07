71d30279c07641a8665a22c670ca8d453ee5c259
pgvaghela <pgvaghela20@gmail.com>
2024-12-06 19:43:21 -0700

    added jar for running ease, removed useless file

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -1,7 +1,7 @@
-import frontend.GameUI;
-
+package frontend;
 public class Main {
     public static void main(String[] args) {
         new GameUI();
+        System.out.println("Game is running!");
     }
 }
